1abf25397afc53410659a0f63f458cf323131fb3
Dhananjaya <dhananjaya.m258@gmail.com>
2026-05-26 12:19:02 +0530

    Add files via upload

---
+++ prog6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string addr,clg, usn, name;
+    int age;
+    cout << "NAME :";
+    cin >> name;
+    cout << "AGE :";
+    cin >> age;
+    cout << "ADDRESS :";
+    cin >> addr;
+    cout << "USN :";
+    cin >> usn;
+    cout << "COLLEGE :";
+    cin >> clg;
+    cout << "NAME :"<< name<<endl;
+    cout << "AGE :"<<age<<endl;
+    cout << "ADDRESS :"<<addr<<endl;
+    cout << "USN :"<<usn<<endl;
+    cout << "COLLEGE :"<<clg<<endl;
+    return 0;
+}
